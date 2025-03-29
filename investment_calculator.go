bab63b8fa91d90978328db4e43d32c92981b486c
ThomasRog3rs <Mr.Thomas.Rogers@proton.me>
2025-03-29 23:33:02 +0000

    Scan for user input

--- investment_calculator.go
+++ investment_calculator.go
@@ -11,6 +11,12 @@ func main() {
 	expectedReturnRate := 5.5
 	var years float64 = 10
 
+	fmt.Print("Please input your investment amount: ")
+	_, err := fmt.Scan(&investmentAmount)
+	if err != nil {
+		fmt.Println("Failed to read input, defaulting investement amount to 1000")
+	}
+
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
