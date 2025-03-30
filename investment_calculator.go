275d213b0a4d0f7b3da7db4d9df841a63e2ef6f6
ThomasRog3rs <Mr.Thomas.Rogers@proton.me>
2025-03-30 21:35:59 +0100

    profit calculation

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,25 +1,53 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// func main() {
+// 	const inflationRate = 2.5
+// 	var investmentAmount float64 = 1000
+// 	expectedReturnRate := 5.5
+// 	var years float64 = 10
+
+// 	fmt.Print("Please input your investment amount: ")
+// 	_, err := fmt.Scan(&investmentAmount)
+// 	if err != nil {
+// 		fmt.Println("Failed to read input, defaulting investement amount to 1000")
+// 	}
+
+// 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+// 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+// 	fmt.Println(futureValue)
+// 	fmt.Println(futureRealValue)
+// }
+
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	expectedReturnRate := 5.5
-	var years float64 = 10
+	var revenue float64
+	var expenses float64
+	var taxRate float64
 
-	fmt.Print("Please input your investment amount: ")
-	_, err := fmt.Scan(&investmentAmount)
-	if err != nil {
-		fmt.Println("Failed to read input, defaulting investement amount to 1000")
-	}
+	fmt.Print("Please enter revenue made: ")
+	fmt.Scan(&revenue)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Print("Please enter your expenses: ")
+	fmt.Scan(&expenses)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	fmt.Print("Please enter your taxRate: ")
+	fmt.Scan(&taxRate)
+
+	earningsBeforeTax := revenue - expenses
+	profit := earningsBeforeTax * (1 - (taxRate / 100))
+
+	ratio := earningsBeforeTax / profit
+
+	fmt.Printf("Earnings Before Tax (EBT): $%.2f\n", earningsBeforeTax)
+	fmt.Printf("Profit After Tax: $%.2f\n", profit)
+	fmt.Printf("EBT/Profit Ratio: %.2f\n", ratio)
 }
